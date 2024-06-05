d2a80fce93d8a868a5fce14623e1d58026e93442
ryanh <ryanhinson118@mail.com>
2024-06-05 19:00:02 -0400

    Updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Nav from './Nav';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Header = () => {
+export default function Header() {
   return (
-    <header>
-      <h1><Link to="/">Ryan Hinson</Link></h1>
+    <header className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <h1 className="navbar-brand">
+          <Link to="/" className="text-dark text-decoration-none">
+            Ryan Hinson
+          </Link>
+        </h1>
+        <Nav />
+      </div>
     </header>
   );
 };
-
-export default Header;
